7fec472438220e80756ba9debac9ed2719c796d2
Keshire <keshire_the_chaotic@hotmail.com>
2019-02-23 21:08:38 -0600

    Full Project Commit

---
+++ Assets/HandManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    Rigidbody2D hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hand = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hand.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
