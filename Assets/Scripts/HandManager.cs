fc10ff3bec8e7b1b123f9115a3e575961f2c4623
Keshire <keshire_the_chaotic@hotmail.com>
2019-02-24 13:05:47 -0600

    IK Targeting code
    Keeping it simple at 4 sword positions.
    Need to adjust the IK so hand positions sync better.

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class HandManager : MonoBehaviour
 {
-    Rigidbody2D hand;
+
+    Rigidbody2D handPhysics;
+    public Transform[] targets;
+    //public float speed = 0.001f;
+    public float minDistance = 0.0001f;
+
     // Start is called before the first frame update
     void Start()
     {
-        hand = GetComponent<Rigidbody2D>();
+        handPhysics = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        hand.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        Vector3 target = GetClosestTarget(targets).position;
+        Vector3 hand = transform.transform.position;
+        Vector3 targetDirection = (target - hand).normalized;
+
+        if (Vector3.Distance(target, hand) > minDistance)
+            transform.position = Vector2.MoveTowards(hand, targetDirection, 0.01f);
+    }
+
+    private void FixedUpdate()
+    {
+        //handPhysics.MovePosition(hand + targetDirection * speed * Time.fixedDeltaTime);
+
+    }
+
+    Transform GetClosestTarget(Transform[] targets)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 m = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        foreach (Transform potentialTarget in targets)
+        {
+            Vector3 directionToTarget = potentialTarget.position - m;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
     }
 }
